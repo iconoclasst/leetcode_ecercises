748116dd4f110805447589935f31b238122a1436
Davi Bezerra <davi09bezerra70@gmail.com>
2024-06-17 16:14:41 -0300

    add files

--- roman_to_integer/roman_to_integer.py
+++ roman_to_integer/roman_to_integer.py
@@ -1,25 +1,26 @@
 # numero romano 
 
-s = {'I' : 1,
-    'V' : 5,
-    'X' : 10,
-    'L' : 50,
-    'C' : 100, 
-    'D' : 500,
-    'M' : 1000}
+def roman_to_integer(value):
+    s = {'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100, 
+        'D' : 500,
+        'M' : 1000}
 
-value = input("Numero romano: ")
+    #entrada = I X L V
+    #value : [0, 1, 2, 3]
+    sum = 0
 
-#entrada = I X L V
-#value : [0, 1, 2, 3]
+    for i in range(len(value)):
+        
+        if i + 1 < len(value) and s[value[i]] < s[value[i + 1]]:
+            sum -= s[value[i]]
+        else:
+            sum += s[value[i]]
 
-sum = 0
+    return sum
 
-for i in range(len(value)):
-    
-    if i + 1 < len(value) and s[value[i]] < s[value[i + 1]]:
-        sum -= s[value[i]]
-    else:
-        sum += s[value[i]]
-
-print(sum)
+value = input("add a roman number: ")
+print(roman_to_integer(value))
