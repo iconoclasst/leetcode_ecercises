861657d160a4f886291657ebb7ccd471cdc71056
Davi Bezerra <davi09bezerra70@gmail.com>
2024-06-17 16:11:41 -0300

    add files

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -4,19 +4,6 @@ nums = [2,7,11,15]
 nums = [3, 3]
 target = 6
 
-# index = []
-
-
-# for i in range(len(nums)):
-#     hash[nums[i]] = target - nums[i]
-
-# for i in range(len(nums)):
-#     for j in range(i + 1, len(nums)):
-#         if hash[nums[i]] == nums[j]:
-#             index.append(i)
-#             index.append(j)
-#             break
-
 def two_sum(nums, target):
     hash = {}
     for i in range(len(nums)):
